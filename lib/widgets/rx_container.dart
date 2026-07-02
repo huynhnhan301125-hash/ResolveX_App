46fb84aa71b68423f580fad0b7b5fd817faf2013
HuynhNhan_301125 <huynhnhan301125@gmail.com>
2026-07-02 10:08:14 +0700

    Thêm giới hạn độ dài cho widget Text, thuộc tính để định hình giao diện

--- lib/widgets/rx_container.dart
+++ lib/widgets/rx_container.dart
@@ -5,120 +5,98 @@ import 'package:intl/intl.dart';
 class RXContainer extends StatelessWidget {
   final ReportModel reportModel;
   final VoidCallback onTap;
-  final EdgeInsetsGeometry margin;
-  final EdgeInsetsGeometry padding;
 
   const RXContainer({
     super.key,
     required this.reportModel,
-    this.margin = const EdgeInsets.all(10),
-    this.padding = const EdgeInsets.all(10),
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: margin,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.3),
-            blurRadius: 6,
-            spreadRadius: 1,
-          ),
-        ],
-      ),
-
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: onTap,
-          child: Padding(
-            padding: padding,
-            // Sử dụng IntrinsicHeight để ép các con trong Row có chiều cao bằng nhau (theo thằng cao nhất).
-            // Nhờ đó, Column bên phải mới có đủ chiều cao để thực hiện spaceBetween (đẩy status lên đầu và time xuống cuối).
-            child: IntrinsicHeight(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
+    return Card(
+      clipBehavior: Clip.antiAlias,// Giúp các con bên trong bị cắt theo bo góc của Card
+      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(15),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: IntrinsicHeight(
+            child: Row(
+              children: [
+                // Cột trái: Thông tin chính
+                Expanded(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "Id: ${reportModel.empId}",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
+                        "Mã NV: ${reportModel.empId}",
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                        maxLines: 1,// Chỉ hiện 1 dòng
+                        overflow: TextOverflow.ellipsis,// Nếu dài quá thì hiện dấu ...
                       ),
-                      SizedBox(height: 10),
-                      Text("Phòng: ${reportModel.problemRoom}"),
-                      SizedBox(height: 10),
-                      Text("Vấn đề: ${reportModel.problemType.label}"),
-                      SizedBox(height: 10),
-                      Text(
-                        "Mức độ: ${reportModel.level.label}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: reportModel.level.color,
-                        ),
-                      ),
-                    ],
-                  ),
-              
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: reportModel.status.color,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withValues(alpha: 0.3),
-                              blurRadius: 4,
-                              spreadRadius: 1,
-                            )
-                          ]
-                        ),
-              
-                        child: Text(
-                          reportModel.status.label,
-                          style: TextStyle(
-                            fontSize: 11,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      // kk:mm ép định dạng về 24h (09:00)
-                      Icon(
-                        reportModel.problemType.icon,
-                        size: 55,
-                        color: reportModel.problemType.color,
-                        shadows: [
-                          Shadow(
-                            color: Colors.black.withValues(alpha: 0.5),
-                            blurRadius: 6,
-                            offset: Offset(4, 4),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
+                          //const Icon(Icons.meeting_room, size: 16, color: Colors.grey),
+                          //const SizedBox(width: 4),
+                          // Bọc Expanded để tên phòng dài không làm vỡ Row
+                          Expanded(
+                              child: Text("Phòng: ${reportModel.problemRoom}",
+                              maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              
                           ),
                         ],
                       ),
+                      const SizedBox(height: 4),
                       Text(
-                        DateFormat(
-                          'kk:mm - dd/MM',
-                        ).format(reportModel.reportDate),
-                        style: TextStyle(color: Colors.black, fontSize: 10),
+                        "Loại: ${reportModel.problemType.label}",
+                      ),
+                      const SizedBox(height: 4),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                        decoration: BoxDecoration(
+                          color: reportModel.level.color.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(4),
+                          border: Border.all(color: reportModel.level.color.withOpacity(0.3))
+                        ),
+                        child: Text(
+                          "Ưu tiên: ${reportModel.level.label}",
+                          style: TextStyle(color: reportModel.level.color, fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+                // Cột phải: Status và Icon
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: reportModel.status.color,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Text(
+                        reportModel.status.label,
+                        style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Icon(reportModel.problemType.icon, size: 40, color: reportModel.problemType.color.withOpacity(0.8)),
+                    Text(
+                      DateFormat('HH:mm - dd/MM').format(reportModel.reportDate),
+                      style: const TextStyle(fontSize: 10, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
