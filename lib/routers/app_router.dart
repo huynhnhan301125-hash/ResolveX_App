36eb3d98b19474ab8746a8103f4cea77802456b1
HuynhNhan_301125 <huynhnhan301125@gmail.com>
2026-07-02 09:42:11 +0700

    Tới ưu codd, thay đổi cách chuyển trang (gọi tên thay vì gọi trực tiếp đường dẫn)

--- lib/routers/app_router.dart
+++ lib/routers/app_router.dart
@@ -1,40 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:resolvex_mobile_app/models/report_model.dart';
 import 'package:resolvex_mobile_app/screens/auth_screen.dart';
 import 'package:resolvex_mobile_app/screens/detail_report_screen.dart';
 import 'package:resolvex_mobile_app/screens/forgot_password_screen.dart';
 import 'package:resolvex_mobile_app/screens/main_employee_screen.dart';
 import 'package:resolvex_mobile_app/screens/update_password_screen.dart';
+import 'package:flutter/material.dart';
 
-// GoRouter: Bộ điều phối trung tâm: Quản lý toàn bộ lộ trình của App
-// initiallocation: Điểm dừng chân đầu tiên khi vừa mở App (mặc định là Login)
-// routes: Danh sách các tuyến đường hợp lệ trong toàn bộ hệ thống
-// GoRoute: Định nghĩa 1 con đường: path là địa chỉ, builder là cái nhà (màn hình)
+// Tạo lớp chứa tên đường dấn tránh bị gõ sai
+class RoutePaths {
+  static const String login = '/login';
+  static const String forgotPassword = '/forgot-password';
+  static const String updatePassword = '/update-password';
+  static const String mainEmployee = '/main-employee';
+  static const String detailReport = '/detail-report';
+}
+
 final GoRouter goRouter = GoRouter(
+  initialLocation: RoutePaths.login,
+
   routes: [
     GoRoute(
-      path: '/login_screen',
+      path: RoutePaths.login,
+      name: 'Login',
       builder: (context, state) => const AuthScreen(),
     ),
     GoRoute(
-      path: '/forgot_password_screen',
+      path: RoutePaths.forgotPassword,
+      name: 'forgot-password', // Dùng name để điều hướng an toàn hơn
       builder: (context, state) => const ForgotPasswordScreen(),
     ),
     GoRoute(
-      path: '/update_password_screen',
+      path: RoutePaths.updatePassword,
+      name: 'update-password',
       builder: (context, state) {
-        // Trích xuất "thẻ bài" từ extra, nếu không có (null) thì mặc định là false (luồng đổi pass bình thường)
         final isFromOTP = state.extra as bool? ?? false;
         return UpdatePassword(isFromOTP: isFromOTP);
       },
     ),
     GoRoute(
-      path: '/main_emp_screen',
+      path: RoutePaths.mainEmployee,
+      name: 'main-employee',
       builder: (context, state) => const MainEmployeeScreen(),
     ),
     GoRoute(
-      path: '/-',
-      builder: (context, state) => const DetailReportScreen(),
+      path: RoutePaths.detailReport,
+      name: 'detail-report',
+      builder: (context, state) {
+        //Ép kiểu an toàn để tránh văng app
+        if (state.extra is ReportModel) {
+          final report = state.extra as ReportModel;
+          return DetailReportScreen(report: report);
+        }
+        return const Scaffold(
+          body: Center(child: Text('Dữ liệu báo cáo bị lỗi!')),
+        );
+      },
     ),
   ],
-  initialLocation: '/login_screen',
+  // Thêm trang báo lỗi để app không bi treo nếu lạc đường
+  errorBuilder: (context, state) =>
+  const Scaffold(body: Center(child: Text('Trang không tồn tại!'))),
 );
